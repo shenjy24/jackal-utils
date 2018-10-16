3a36e74f1b77eebb8f73f6188744a7d9780803de
shenjy <shenjiayun@hzyushi.cn>
2018-10-16 15:27:49 +0800

    AES工具类

---
+++ src/main/java/com/jonas/AesUtils.java
@@ -0,0 +1,119 @@
+package com.jonas;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+/**
+ * 【 AES加解密工具 】
+ *
+ * @author shenjy 2018/10/16
+ */
+public class AesUtils {
+    /**
+     * AES加密
+     * @param content
+     * @param password
+     * @return
+     */
+    public static String encrypt(String content, String password) {
+        try {
+            KeyGenerator keygen = KeyGenerator.getInstance("AES");
+            SecureRandom random=SecureRandom.getInstance("SHA1PRNG");
+            random.setSeed(password.getBytes());
+            keygen.init(128, random);
+            SecretKey secretKey = keygen.generateKey();
+            byte[] enCodeFormat = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            byte[] byteContent = content.getBytes("utf-8");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] result = cipher.doFinal(byteContent);
+            return parseByte2Hex(result);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * AES解密
+     * @param content
+     * @param password
+     * @return
+     */
+    public static String decrypt(String content, String password) {
+        try {
+            KeyGenerator keygen = KeyGenerator.getInstance("AES");
+            SecureRandom random=SecureRandom.getInstance("SHA1PRNG");
+            random.setSeed(password.getBytes());
+            keygen.init(128, random);
+            SecretKey secretKey = keygen.generateKey();
+            byte[] enCodeFormat = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] result = cipher.doFinal(parseHex2Byte(content));
+            return new String(result);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将二进制转换成16进制
+     * @param buf
+     * @return
+     */
+    private static String parseByte2Hex(byte buf[]) {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < buf.length; i++) {
+            String hex = Integer.toHexString(buf[i] & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+            sb.append(hex.toUpperCase());
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 将16进制转换为二进制
+     * @param hex
+     * @return
+     */
+    private static byte[] parseHex2Byte(String hex) {
+        if (hex.length() < 1) {
+            return null;
+        }
+        byte[] result = new byte[hex.length()/2];
+        for (int i = 0;i< hex.length()/2; i++) {
+            int high = Integer.parseInt(hex.substring(i*2, i*2+1), 16);
+            int low = Integer.parseInt(hex.substring(i*2+1, i*2+2), 16);
+            result[i] = (byte) (high * 16 + low);
+        }
+        return result;
+    }
+}
--- src/test/java/com/jonas/AppTest.java
+++ src/test/java/com/jonas/AppTest.java
@@ -1,20 +1,32 @@
 package com.jonas;
 
-import static org.junit.Assert.assertTrue;
-
+import com.alibaba.fastjson.JSON;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testMD5() {
+        String pwd = "1234567";
+        String md5Pwd = "05d55d438b6c11083f29f94505a241f1888dd3049f985f05";
+        Assert.assertTrue(MD5Utils.verifySaltMd5(pwd, md5Pwd));
+    }
+
+    @Test
+    public void testAes() {
+        String content = "{\"name\" : \"Tom\"}";
+        String password = "123456";
+
+        //加密
+        System.out.println("加密前：" + JSON.toJSONString(content));
+        String encryptResult = AesUtils.encrypt(content, password);
+        System.out.println("加密后：" + encryptResult);
+        //解密
+        String decryptResult = AesUtils.decrypt(encryptResult, password);
+        System.out.println("解密后：" + decryptResult);
     }
 }
