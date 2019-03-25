20aa1a4828e5f64a32cc07e81761adb369dd8e79
shenjy <shenjiayun@hzyushi.cn>
2019-03-25 14:35:34 +0800

    验证密码

--- src/main/java/com/jonas/security/ValidateUtils.java
+++ src/main/java/com/jonas/security/ValidateUtils.java
@@ -16,6 +16,12 @@ public class ValidateUtils {
     public static final String REGEX_ACCOUNT = "^(?![0-9]+$)[0-9A-Za-z]{5,20}$";
 
     /**
+     * 正则表达式：验证密码
+     * 6-18 不包含中文
+     */
+    public static final String REGEX_PWD = "^((?![\\u4e00-\\u9fa5]).){6,18}$";
+
+    /**
      * 校验自定义账户
      *
      * @param account
@@ -25,20 +31,18 @@ public class ValidateUtils {
         return Pattern.matches(REGEX_ACCOUNT, account);
     }
 
+    /**
+     * 校验密码
+     *
+     * @param pwd
+     * @return
+     */
+    public static boolean isPassword(String pwd) {
+        return Pattern.matches(REGEX_PWD, pwd);
+    }
+
     public static void main(String[] args) {
-        String account1 = "12345";
-        System.out.println(isAccount(account1));
-
-        String account2 = "123a45";
-        System.out.println(isAccount(account2));
-
-        String account3 = "a12345";
-        System.out.println(isAccount(account3));
-
-        String account4 = "12345a";
-        System.out.println(isAccount(account4));
-
-        String account5 = "ascdkd";
-        System.out.println(isAccount(account5));
+        String pwd = "人人车窗好不好";
+        System.out.println(isPassword(pwd));
     }
 }
