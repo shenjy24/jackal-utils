0146633850c6db00cd6d721be6d9f0f50f1a02e5
shenjy <shenjy24@gmail.com>
2019-12-19 17:05:29 +0800

    文件工具方法

--- src/main/java/com/jonas/file/FileCreator.java
+++ src/main/java/com/jonas/file/FileCreator.java
@@ -15,19 +15,60 @@ import java.io.InputStream;
  */
 public class FileCreator {
 
+    /**
+     * 文件复制
+     *
+     * @param targetFileName 目标文件名
+     * @param sourceFile     源文件
+     * @return
+     */
     public static boolean copyFile(String targetFileName, File sourceFile) {
         File file = new File(targetFileName);
         if (file.exists() || !sourceFile.exists()) {
             return false;
         }
-        InputStream initialStream = null;
         try {
-            initialStream = FileUtils.openInputStream(sourceFile);
+            InputStream initialStream = FileUtils.openInputStream(sourceFile);
             FileUtils.copyInputStreamToFile(initialStream, file);
         } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
         return true;
+    }
+
+    /**
+     * 创建文件及其父目录
+     *
+     * @param file
+     * @return
+     */
+    public static File createFile(File file) {
+        if (null == file) {
+            return null;
+        }
+        if (!file.exists()) {
+            mkParentDirs(file);
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return file;
+    }
+
+    /**
+     * 创建所给文件或目录的父目录
+     *
+     * @param file 文件或目录
+     * @return 父目录
+     */
+    public static File mkParentDirs(File file) {
+        final File parentFile = file.getParentFile();
+        if (null != parentFile && !parentFile.exists()) {
+            parentFile.mkdirs();
+        }
+        return parentFile;
     }
 }
--- src/main/java/com/jonas/object/BeanCopier.java
+++ src/main/java/com/jonas/object/BeanCopier.java
@@ -1,7 +1,9 @@
 package com.jonas.object;
 
+import cn.hutool.core.util.ObjectUtil;
 import com.alibaba.fastjson.JSON;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -22,5 +24,16 @@ public class BeanCopier {
     public static <T> List<T> copyList(List source, Class<T> clazz) {
         List<T> target = JSON.parseArray(JSON.toJSONString(source), clazz);
         return target;
+    }
+
+    /**
+     * 深克隆
+     *
+     * @param source
+     * @param <T>
+     * @return
+     */
+    public static <T extends Serializable> T deepClone(T source) {
+        return ObjectUtil.cloneByStream(source);
     }
 }
