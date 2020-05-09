c632f63835e5cbe5599d6b5034b8fadae26d777a
shenjy <shenjy24@gmail.com>
2020-05-09 19:46:19 +0800

    feat: 增加随机数工具

---
+++ src/main/java/com/jonas/common/RandomUtils.java
@@ -0,0 +1,23 @@
+package com.jonas.common;
+
+import cn.hutool.core.util.RandomUtil;
+
+import java.util.Random;
+
+/**
+ * 随机数工具
+ */
+public class RandomUtils {
+
+    private static Random random = new Random();
+
+    public static int nextInt(int start, int end) {
+        return (int) Math.floor(Math.random() * (end - start + 1) + start);
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(nextInt(1, 3));
+        }
+    }
+}
