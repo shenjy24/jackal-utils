339ae064cccd7ffa1634a2c93b9630d13c332b5c
shenjy <shenjy24@gmail.com>
2021-09-02 17:52:26 +0800

    整数x和y是否异号

--- src/main/java/com/jonas/common/NumberUtils.java
+++ src/main/java/com/jonas/common/NumberUtils.java
@@ -8,12 +8,31 @@ import java.util.regex.Pattern;
  * @description
  */
 public class NumberUtils {
-    public static boolean isNumeric(String str){
+
+    /**
+     * 判断是否是数字字符串
+     *
+     * @param str 字符串
+     * @return 是否是数字字符串
+     */
+    public static boolean isNumeric(String str) {
         Pattern pattern = Pattern.compile("^[1-9]\\d*$");
         return pattern.matcher(str).matches();
     }
 
+    /**
+     * 整数x和y是否异号
+     *
+     * @param x 整数X
+     * @param y 整数Y
+     * @return 是否异号
+     */
+    public static boolean isOppositeSign(int x, int y) {
+        return (x ^ y) < 0;
+    }
+
     public static void main(String[] args) {
         System.out.println(isNumeric("-1"));
+        System.out.println(isOppositeSign(0, -1));
     }
 }
--- src/main/java/com/jonas/common/StringUtils.java
+++ src/main/java/com/jonas/common/StringUtils.java
@@ -55,7 +55,7 @@ public class StringUtils {
 
     /**
      * 格式化文本，使用 <delimiter>varName<delimiter> 占位<br>
-     * map = {a: "aValue", b: "bValue"} format("%a% and %b%", map， "%") ---=》 aValue and bValue
+     * map = {a: "aValue", b: "bValue"} format("%a% and %b%", map, "%") ---=》 aValue and bValue
      *
      * @param template 文本模板，被替换的部分用 {key} 表示
      * @param map 参数值对
