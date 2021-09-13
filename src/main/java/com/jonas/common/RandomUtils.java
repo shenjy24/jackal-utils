5b7faf64557e564957bf2a0c3f003b57e5658577
shenjy <shenjy24@gmail.com>
2021-09-13 20:13:39 +0800

    随机方法

--- src/main/java/com/jonas/common/RandomUtils.java
+++ src/main/java/com/jonas/common/RandomUtils.java
@@ -7,22 +7,33 @@ import java.util.Random;
  */
 public class RandomUtils {
 
-    private static Random random = new Random();
+    private static final Random random = new Random();
 
     /**
      * 获取区间[start, end]的随机数
      *
-     * @param start 起始值
+     * @param begin 起始值
      * @param end   截止值
      * @return 随机数
      */
-    public static int nextInt(int start, int end) {
-        return (int) Math.floor(Math.random() * (end - start + 1) + start);
+    public static int nextInt(int begin, int end) {
+        return (int) Math.floor(Math.random() * (end - begin + 1) + begin);
+    }
+
+    /**
+     * 获取区间[start, end]的随机数
+     *
+     * @param begin 起始值
+     * @param end   截止值
+     * @return 随机数
+     */
+    public static int randomInt(int begin, int end) {
+        return random.nextInt(end - begin + 1) + begin;
     }
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println(nextInt(1, 3));
+            System.out.println(randomInt(1, 3));
         }
     }
 }
